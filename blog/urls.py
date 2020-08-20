ab7a83d9144939d0292483d9e8dce0c22856ae77
inbal <inbalroz91@gmail.com>
2020-08-20 22:27:50 +0300

    first edit

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [path('', views.home, name='blog-home'),
-    path('', views.home, name='blog-home'),
+    path('', views.home, name='blog-home2'),
     path('Ainstructions/', views.instructions, name='blog-instructions'),
     path('Binstructions/', views.instructions, name='blog-Binstructions'),
     path('blank/', views.blank, name='blog-blank'),
