90c831d99f384d7688eeb8e73ef123ac8ae6d1da
inbal <inbalroz91@gmail.com>
2020-08-23 09:50:48 +0300

    delete users utils

--- users/views.py
+++ users/views.py
@@ -10,7 +10,6 @@ import json
 import os.path
 from django.views.generic import View
 
-from users.utils import render_to_pdf #created in step 4
 
 
 
@@ -104,24 +103,3 @@ def confirmation(request):
 
 
 
-class GeneratePDF(View):
-    def get(self, request, *args, **kwargs):
-        template = get_template('invoice.html')
-        context = {
-            "invoice_id": 123,
-            "customer_name": "John Cooper",
-            "amount": 1399.99,
-            "today": "Today",
-        }
-        html = template.render(context)
-        pdf = render_to_pdf('invoice.html', context)
-        if pdf:
-            response = HttpResponse(pdf, content_type='application/pdf')
-            filename = "Invoice_%s.pdf" %("12341231")
-            content = "inline; filename='%s'" %(filename)
-            download = request.GET.get("download")
-            if download:
-                content = "attachment; filename='%s'" %(filename)
-            response['Content-Disposition'] = content
-            return response
-        return HttpResponse("Not found")
